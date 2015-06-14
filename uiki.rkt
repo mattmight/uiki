b6fce72987e546f2c91eeac2fbd76f97d6bb5366
Matt Might <matt@might.net>
2015-06-13 20:27:02 -0600

    Minor tweaks to copyright notice.

--- uiki.rkt
+++ uiki.rkt
@@ -1,8 +1,7 @@
 #lang racket
 
-
-; UIKI: A simple academic wiki.
-; Copyright (C) 2005 Matthew Might
+; Uiki: A simple academic wiki.
+; Copyright (C) 2015 Matthew Might
 
 ; This program is free software: you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
